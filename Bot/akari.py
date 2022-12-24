c5c22fa9b3539e132f597340ea4413c0b0543fc7
No767 <73260931+No767@users.noreply.github.com>
2022-12-23 23:51:08 -0800

    Make the DB connection first, not spam PostgreSQL w/ requests

--- Bot/akari.py
+++ Bot/akari.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import urllib.parse
 
 import discord
 import uvloop
@@ -9,18 +10,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+POSTGRES_USER = os.getenv("Postgres_User")
+POSTGRES_PASSWORD = urllib.parse.quote_plus(os.getenv("Postgres_Password"))
+POSTGRES_HOST = os.getenv("Postgres_Host")
+POSTGRES_PORT = os.getenv("Postgres_Port")
+POSTGRES_DB = os.getenv("Postgres_Akari_DB")
+CONNECTION_URI = f"asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+MODELS = [
+    "akari_tags_utils.models",
+    "akari_admin_logs.models",
+    "akari_servers.models",
+    "akari_modmail.models",
+]
+
 DISCORD_BOT_TOKEN = os.getenv("Akari_Dev_Token")
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
-bot = AkariCore(intents=intents)
+bot = AkariCore(uri=CONNECTION_URI, models=MODELS, intents=intents)
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] | %(asctime)s >> %(message)s",
     datefmt="[%m/%d/%Y] [%I:%M:%S %p %Z]",
 )
-logging.getLogger("tortoise").setLevel(logging.WARNING)
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
--- Bot/akaricore.py
+++ Bot/akaricore.py
@@ -1,9 +1,14 @@
+import asyncio
 import logging
 import os
 import sys
 from pathlib import Path
+from typing import List
 
 import discord
+from discord.ext import tasks
+from tortoise import BaseDBAsyncClient, Tortoise, connections
+from tortoise.exceptions import DBConnectionError
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,8 +27,14 @@ sys.path.append(str(libPath))
 class AkariCore(discord.Bot):
     """The core of Akari, but this time it's subclassed"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, uri: str, models: List, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.uri = uri
+        self.models = models
+        self.connected_db = asyncio.Event()
+        self.connect_db.add_exception_type(TimeoutError)
+        self.connect_db.add_exception_type(DBConnectionError)
+        self.connect_db.start()
         self.load_cogs()
 
     def load_cogs(self):
@@ -33,6 +44,24 @@ class AkariCore(discord.Bot):
         for cogs in os.listdir(cogsPath):
             if cogs.endswith(".py"):
                 self.load_extension(f"Cogs.{cogs[:-3]}", store=False)
+
+    @tasks.loop(count=1)
+    async def connect_db(self):
+        try:
+            await Tortoise.init(db_url=self.uri, modules={"models": self.models})
+            conn: BaseDBAsyncClient = connections.get("default")
+            await conn.create_connection(with_db=True)
+            self.connected_db.set()
+            logging.info("Connected to database")
+        except TimeoutError:
+            logging.error("Failed to connect to PostgreSQL. Retrying in 15 seconds")
+            await asyncio.sleep(15)
+            self.connect_db.restart()
+
+    @connect_db.after_loop
+    async def connTeardown(self):
+        if self.connect_db.is_being_cancelled():
+            await connections.close_all()
 
     async def on_ready(self):
         logging.info(f"{self.user.name} is ready!")
